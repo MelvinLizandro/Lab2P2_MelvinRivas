a7e35df4a07a1fe68d923df56126a387998f47f5
MelvinLizandro <melvinrivas@unitec.edu>
2024-01-26 14:54:00 -0600

    4 avance

--- Lab2P2_MelvinRivas/src/lab2p2_melvinrivas/Lab2P2_MelvinRivas.java
+++ Lab2P2_MelvinRivas/src/lab2p2_melvinrivas/Lab2P2_MelvinRivas.java
@@ -4,6 +4,7 @@
  */
 package lab2p2_melvinrivas;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -18,18 +19,28 @@ public class Lab2P2_MelvinRivas {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
+        ArrayList<Libros> lib = new ArrayList<>(); //arreglo de la clase libros
+        ArrayList<Articulo> arti = new ArrayList<>(); //arreglo de la clase articulos
+        ArrayList<Cursolinea> curso = new ArrayList<>(); //arreglo de la clase curso en linea
+        ArrayList<Confevirtuales> confe = new ArrayList<>(); //arreglo de la clase Conferencia virtuales
+        ArrayList<Usuario> user = new ArrayList<>(); // arreglo de la clase usuario
+        ArrayList union = new ArrayList(); //Aqui van a ir todo los arreglos juntos 
+        
         int op = 0; // op == opciones
         do {//inicio do while
             
-            System.out.println("");
-            System.out.println("");
-            System.out.println("");
-            System.out.println("");
-            System.out.println("");
-            System.out.println("");
+            System.out.println("***Bienvenidos a nuestro sistema de inventarios***");
+            System.out.println("1. Listar recurso");
+            System.out.println("2. Crear recurso");
+            System.out.println("3. Eliminar recurso");
+            System.out.println("5. Salir ");
+            System.out.print("Que desea realizar: ");
+            op = input.nextInt();
             
             switch (op) { //inicio switch
-                case 1: {
+                case 1: {// Listar recursos
+                    
+                    union.clear();
                     
                     break;
                 }
